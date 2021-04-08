ee86386c2eab23ec2a174405dd662195ef2852b7
srmtwenty <srmcho@hotmail.com>
2021-04-07 21:21:40 -0700

    ver18i

--- src/views/WorksCode.js
+++ src/views/WorksCode.js
@@ -37,53 +37,54 @@ export default(props)=>{
         {/* <div style={{backgroundImage: `url(${bg1da1})`}}> */}
             <div style={{backgroundColor: "rgb(223, 243, 255)", border: "yellow none 2px", marginLeft:"auto", marginRight:"auto", width: "1900px"}}>
                 <Menu/>
-            
-                <div style={{textAlign: "center"}}>
-                    <div>
+                <div style={{width: "1000px", height: "auto" ,marginLeft: "auto", marginRight: "auto", backgroundColor: "white", paddingTop: "5px"}}>
+                    <div style={{textAlign: "center"}}>
                         <div>
-                            <h2>EasyShop - Simple and intuitive E-commerce website(Python/Django)</h2>
+                            <div>
+                                <h2>EasyShop - Simple and intuitive E-commerce website(Python/Django)</h2>
+                            </div>
+                            <SimpleImageSlider
+                                width={800}
+                                height={504}
+                                images={images1}
+                                style={{border: "black outset 2px", marginTop:"20px", marginBottom:"40px", marginLeft:"auto", marginRight:"auto"}}
+                                navStyle={1}
+                                showNavs={true}
+                                showBullets={true}
+                            />   
                         </div>
-                        <SimpleImageSlider
-                            width={800}
-                            height={504}
-                            images={images1}
-                            style={{border: "black outset 2px", marginTop:"20px", marginBottom:"40px", marginLeft:"auto", marginRight:"auto"}}
-                            navStyle={1}
-                            showNavs={true}
-                            showBullets={true}
-                        />   
-                    </div>
-                
-                    <div>
-                        <div>
-                            <h2>AQI-Maps - Real time air quality map(MERN)</h2>
-                        </div>
-                        <SimpleImageSlider
-                            width={800}
-                            height={504}
-                            images={images2}
-                            style={{border: "black outset 1px", marginTop:"20px", marginBottom:"40px",marginLeft:"auto", marginRight:"auto"}}
-                            navStyle={1}
-                            showNavs={true}
-                            showBullets={true}
-                        />
-                    </div>
-
-                    <div>
-                        <div>
-                            <h2>Friendbook - A clone of Faceboo's social media app(Java)</h2>
-                        </div>
-                        <SimpleImageSlider
-                            width={800}
-                            height={504}
-                            images={images3}
-                            style={{border: "black outset 1px", marginTop:"20px", marginBottom:"40px",marginLeft:"auto", marginRight:"auto"}}
-                            navStyle={1}
-                            showNavs={true}
-                            showBullets={true}
-                        />
-                    </div>
                     
+                        <div>
+                            <div>
+                                <h2>AQI-Maps - Real time air quality map(MERN)</h2>
+                            </div>
+                            <SimpleImageSlider
+                                width={800}
+                                height={504}
+                                images={images2}
+                                style={{border: "black outset 1px", marginTop:"20px", marginBottom:"40px",marginLeft:"auto", marginRight:"auto"}}
+                                navStyle={1}
+                                showNavs={true}
+                                showBullets={true}
+                            />
+                        </div>
+
+                        <div>
+                            <div>
+                                <h2>Friendbook - A clone of Faceboo's social media app(Java)</h2>
+                            </div>
+                            <SimpleImageSlider
+                                width={800}
+                                height={504}
+                                images={images3}
+                                style={{border: "black outset 1px", marginTop:"20px", marginBottom:"40px",marginLeft:"auto", marginRight:"auto"}}
+                                navStyle={1}
+                                showNavs={true}
+                                showBullets={true}
+                            />
+                        </div>
+                        
+                    </div>
                 </div>
             <Footer/>
             </div>
